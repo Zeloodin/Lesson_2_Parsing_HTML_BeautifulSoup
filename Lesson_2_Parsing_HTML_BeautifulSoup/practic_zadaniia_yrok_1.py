c94ba12047f30dee404ada9010aa973db9326bef
Roman Magafurov <roman.mag1997@yandex.ru>
2024-05-23 21:50:00 +0500

    first commit

---
+++ Lesson_2_Parsing_HTML_BeautifulSoup/practic_zadaniia_yrok_1.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup, BeautifulSoup as bs
+from fake_useragent import UserAgent
+from pprint import pprint
+# ua = UserAgent()
+
+# Сбор и разметка данных (семинары)
+# Урок 2. Парсинг HTML. BeautifulSoup
+# Выполнить скрейпинг данных в веб-сайта http://books.toscrape.com/
+# и извлечь информацию о всех книгах на сайте
+# во всех категориях: название, цену, количество товара
+# в наличии (In stock (19 available)) в формате integer, описание.
+#
+# Затем сохранить эту информацию в JSON-файле.
+
+
+
+url = "http://books.toscrape.com/"
+
+# https://books.toscrape.com/index.html
+# https://books.toscrape.com/catalogue/page-1.html
+# https://books.toscrape.com/catalogue/category/books_1/index.html
+# https://books.toscrape.com/catalogue/category/books_1/page-1.html
+
+
+
+
+headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Safari/605.1.15'}
+page = 1
+params = {"catalogue": f"page-{page}"}
+
+session = requests.session()
+
+
+
+
+
+
+
+response = session.get(url,params=params, headers=headers)
+soup = BeautifulSoup(response.text, features="html.parser")
+
+categorys = soup.find_all("div", {"class":"side_categories"})[0]#.find_all("a",{"href":"category/books_1/index.html"})
+
+category_info = {}
+for category in categorys.find_all("li")[1:]:
+    name = category.find("a")
+    print(name.find("href"), url + name.get("href"))
+
+print()

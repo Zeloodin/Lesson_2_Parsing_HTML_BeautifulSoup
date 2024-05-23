a2391b930c3a6ccec2c7cf0d305ce28a459ba58c
Roman Magafurov <roman.mag1997@yandex.ru>
2024-05-24 03:01:59 +0500

    added 8 while True Pre Final

--- Lesson_2_Parsing_HTML_BeautifulSoup/practic_zadaniia_yrok_1.py
+++ Lesson_2_Parsing_HTML_BeautifulSoup/practic_zadaniia_yrok_1.py
@@ -14,7 +14,12 @@ import re
 #
 # Затем сохранить эту информацию в JSON-файле.
 
-
+# GOTO
+# def parser_page(url_link):
+#     print(url_link)
+#
+#     if is_next:
+#         link = f"{page_url_clear}{next_page}"
 
 url = "http://books.toscrape.com/"
 
@@ -46,7 +51,6 @@ for category in categorys.find_all("li")[1:]:
     name = {}
     categ = category.find("a")
 
-    print(categ)
     name["name"] = re.findall(regex_pattern,str(categ))[0]
     name["link"] = (url + categ.get("href"))#.replace("index.html","page-1.html")
 
@@ -58,76 +62,99 @@ currency = {"$":{"name":"dollar", "code":"USD"},
 "¥":{"name":"yuan", "code":"JPY"},
 "₽":{"name":"ruble", "code":"RUB"}}
 
-dict_full_book = {}
+dict_full_books = {}
 
 for category in all_categorys:
     name,link = category.values()
+    is_next = False
+
+    print(f"Переходим в категорию {name}")
 
     page_url = link.split("/")[-1]
     page_url_clear =  link.replace(link.split("/")[-1],"")
 
     end_url = page_url.split(".")[-1]
-    # full_page = page_url.split(".")[0].split("-")
-    #
-    # page = int(full_page[-1])
-    #
-    # full_page = f"{full_page[0]}-{page}"
-    # full_end_url = f"{full_page}.{end_url}"
-    #
-    # full_url = f"{page_url_clear}{full_end_url}"
-    # print(full_url)
 
-    categ_response = session.get(link, headers=headers)
-    categ_soup = BeautifulSoup(categ_response.text, features="html.parser")
+    while True:
 
-    books = categ_soup.find_all("article", {"class": "product_pod"})
-
-    for book in books:
-        all_full_book_info = {}
-
-        base_book = book.find("h3")
-        title = base_book.find('a')["title"]
-        link = f'{base_book.find("a").get("href")}'
-        link = f"{url}catalogue/"+link.replace("../","")
-
-        price_book = book.find("div",{"class":"product_price"})
-        price = price_book.find('p',{'class':'price_color'}).getText()[1:]
-
-        for i in currency.keys():
-            if i in price:
-                price = {"price":float(price.replace(i, "")),"currency": {"name":currency.get(i).get("name"),"code":currency.get(i).get("code")}}
+        categ_response = session.get(link, headers=headers)
+        categ_soup = BeautifulSoup(categ_response.text, features="html.parser")
 
 
-        book_response = session.get(link, headers=headers)
-        book_soup = BeautifulSoup(book_response.text, features="html.parser")
+        if categ_soup.find("li", {"class": "next"}):
+            is_next = True
+            next_page = categ_soup.find("li", {"class": "next"}).find("a").get("href")
+            page_url = link.split("/")[-1].replace("index.html",f"{next_page}")
+            full_page = page_url.split(".")[0].split("-")
+            page = int(full_page[-1])
+            print(f"{page = }")
 
-        info_book = book_soup.find_all("table",{"class":"table"})
-        for inf_bok in info_book:
-            for ibk in inf_bok.find_all("td"):
-                if "In stock" in ibk.getText():
-                    availability = int(re.search("\\(([0-9]+) ",ibk.getText())[1])
+            # full_page = f"{full_page[0]}-{page}"
+            # full_end_url = f"{full_page}.{end_url}"
+            # full_url = f"{page_url_clear}{full_end_url}"
+            # print(full_url)
 
-        find_description_in_book = book_soup.find_all("p")
-        for p_find in find_description_in_book:
-            if "...more" in p_find.getText():
-                description_book = p_find.getText()
+            link = f"{page_url_clear}{next_page}"
 
+            print(f"{page_url = }\n{next_page = }\n{full_page = }\n{page = }\n{link = }")
 
-        all_full_book_info["category"] = name
-        all_full_book_info["title"] = title
-        all_full_book_info["link"] = link
-        all_full_book_info["price"] = price.get("price")
-        all_full_book_info["currency"] = price.get("currency").get("code")
-        all_full_book_info["availability"] = availability if availability else ""
-        all_full_book_info["description"] = description_book if description_book else ""
-        all_full_book_info["id"] = int(re.search("([0-9]+)/index.html",link)[1])
+        books = categ_soup.find_all("article", {"class": "product_pod"})
 
-    print(category)
-    print(all_full_book_info.get("category"))
-    dict_full_book[all_full_book_info.get("id")] = {"category": all_full_book_info["category"],
-                                                    "title": all_full_book_info["title"],
-                                                    "link": all_full_book_info["link"],
-                                                    "description": all_full_book_info["description"],
-                                                    "availability": all_full_book_info["availability"],
-                                                    "price": all_full_book_info["price"],
-                                                    "currency": all_full_book_info["currency"]}
+        for book in books:
+            all_full_book_info = {}
+
+            base_book = book.find("h3")
+            title = base_book.find('a')["title"]
+            link_book = f'{base_book.find("a").get("href")}'
+            link_book = f"{url}catalogue/"+link_book.replace("../","")
+
+            price_book = book.find("div",{"class":"product_price"})
+            price = price_book.find('p',{'class':'price_color'}).getText()[1:]
+
+            for i in currency.keys():
+                if i in price:
+                    price = {"price":float(price.replace(i, "")),"currency": {"name":currency.get(i).get("name"),"code":currency.get(i).get("code")}}
+
+            print(f"Собирается в категории {name}, на странице {page}, книга {title}")
+
+            book_response = session.get(link_book, headers=headers)
+            book_soup = BeautifulSoup(book_response.text, features="html.parser")
+
+            info_book = book_soup.find_all("table",{"class":"table"})
+            for inf_bok in info_book:
+                for ibk in inf_bok.find_all("td"):
+                    if "In stock" in ibk.getText():
+                        availability = int(re.search("\\(([0-9]+) ",ibk.getText())[1])
+
+            find_description_in_book = book_soup.find_all("p")
+            for p_find in find_description_in_book:
+                if "...more" in p_find.getText():
+                    description_book = p_find.getText()
+
+            id_book = int(re.search("([0-9]+)/index.html",link_book)[1])
+
+            print(f"Подготовка книги, {title}, id {id_book}")
+
+            all_full_book_info["category"] = name
+            all_full_book_info["title"] = title
+            all_full_book_info["link"] = link_book
+            all_full_book_info["price"] = price.get("price")
+            all_full_book_info["currency"] = price.get("currency").get("code")
+            all_full_book_info["availability"] = availability if availability else ""
+            all_full_book_info["description"] = description_book if description_book else ""
+            all_full_book_info["id"] = id_book
+
+            dict_full_books[all_full_book_info.get("id")] = {"category": all_full_book_info["category"],
+                                                            "title": all_full_book_info["title"],
+                                                            "link": all_full_book_info["link"],
+                                                            "description": all_full_book_info["description"],
+                                                            "availability": all_full_book_info["availability"],
+                                                            "price": all_full_book_info["price"],
+                                                            "currency": all_full_book_info["currency"]}
+
+            print(f'Книга {all_full_book_info["title"]}, добавлен в словарь, id книги {all_full_book_info["id"]}')
+            print(f"Колличество книг в словаре, {len(dict_full_books)}.")
+            print(end="\n" * 2)
+
+        if not categ_soup.find("li", {"class": "next"}):
+            break
